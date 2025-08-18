1402d52883c1ca322c041c0f6d262bc982b11be5
arsnyan <arsnyan.dev@gmail.com>
2025-08-18 15:50:21 +0300

    Add models for sqlite database

---
+++ GeoMemories/Models/GeoEntry.swift
@@ -0,0 +1,30 @@
+//
+//  GeoEntry.swift
+//  GeoMemories
+//
+//  Created by Арсен Саруханян on 18.08.2025.
+//
+
+import Foundation
+import CoreStore
+
+@MainActor
+class GeoEntry: CoreStoreObject {
+    @Field.Stored("id")
+    var id: UUID = UUID()
+    
+    @Field.Stored("title")
+    var title: String = ""
+    
+    @Field.Stored("description")
+    var description: String = ""
+    
+    @Field.Stored("latitude")
+    var latitude: Double = 0.0
+    
+    @Field.Stored("longitude")
+    var longitude: Double = 0.0
+    
+    @Field.Relationship("media_entries", inverse: \MediaEntry.$linkedGeoEntry)
+    var imageIds: Set<MediaEntry>
+}
